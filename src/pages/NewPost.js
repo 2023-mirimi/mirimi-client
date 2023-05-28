3d2ee0dab988050ae57bd07abd33504042cbb4fd
HA JIN <w2128@e-mirim.hs.kr>
2023-05-28 15:18:20 +0900

    ✨: feat : TextInput 아닌 곳을 탭하면 키보드 내리기

--- src/pages/NewPost.js
+++ src/pages/NewPost.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { StyleSheet,View, Text,TouchableOpacity,Image, TextInput } from "react-native";
+import { StyleSheet,View, Text,TouchableOpacity,Image, TextInput, Pressable, Keyboard } from "react-native";
 import { useState } from "react";
 import DropDownPicker from 'react-native-dropdown-picker';
+import AutoHeightTextInput from "../components/community/AutoHeightTextInput";
 
 const NewPost = ({navigation}) => {
     const [open, setOpen] = useState(false);
@@ -13,77 +14,73 @@ const NewPost = ({navigation}) => {
         {label: '공모전', value: 'contest'}
     ]);
     return(
-        <View style={styles.container}>
-            <View style={styles.header}>
-                <TouchableOpacity onPress={() => navigation.navigate("Root")} style={styles.menuBtn}>
-                    <Image source={require('../assets/back.png')}></Image>
-                </TouchableOpacity>
-                <Text style={styles.headerTxt}>게시글 작성</Text>
-                <TouchableOpacity>
-                    <Text style={styles.uploadBtn}>업로드</Text>
-                </TouchableOpacity>
-            </View>
-            <View>
-                <TextInput style={styles.title} placeholder="제목"/>
-                <DropDownPicker 
-                    open={open}
-                    value={category}
-                    items={items}
-                    setOpen={setOpen}
-                    setValue={setCategory}
-                    setItems={setItems}
-                    disableBorderRadius={true}
-                    onClose={() => console.log(category)} //선택한 카테고리
-                    translation={{
-                        PLACEHOLDER: "카테고리"
-                    }}
-                    dropDownContainerStyle={{
-                        borderBottomColor: '#F3F3F3',
-                        borderRadius: 10,
-                        borderColor: 0,
-                        backgroundColor: '#FAFAFA',
-                    }}
-                    style={{
-                        borderBottomColor: '#F3F3F3',
-                        borderColor: 0,
-                        borderRadius: 0,
-                    }}
-                    labelStyle={{
-                        color: '#414141',
-                        padding: 4,
-
-                    }}
-                    textStyle={{
-                        color: '#414141',
-                        padding: 4,
-                    }}
-                />
+        <Pressable style={{flex: 1}} onPress={() => Keyboard.dismiss()}>
+            <View style={styles.container}>
+                <View style={styles.header}>
+                    <TouchableOpacity onPress={() => navigation.navigate("Root")} style={styles.menuBtn}>
+                        <Image source={require('../assets/back.png')}></Image>
+                    </TouchableOpacity>
+                    <Text style={styles.headerTxt}>게시글 작성</Text>
+                    <TouchableOpacity>
+                        <Text style={styles.uploadBtn}>업로드</Text>
+                    </TouchableOpacity>
+                </View>
                 <View>
-                    <TextInput style={styles.content} placeholder="내용을 입력하세요." multiline={true}  maxLength={500}/>
+                    <TextInput style={styles.title} placeholder="제목"/>
+                    <DropDownPicker 
+                        open={open}
+                        value={category}
+                        items={items}
+                        setOpen={setOpen}
+                        setValue={setCategory}
+                        setItems={setItems}
+                        disableBorderRadius={true}
+                        onClose={() => console.log(category)} //선택한 카테고리
+                        translation={{
+                            PLACEHOLDER: "카테고리"
+                        }}
+                        dropDownContainerStyle={{
+                            borderBottomColor: '#F3F3F3',
+                            borderRadius: 10,
+                            borderColor: 0,
+                            backgroundColor: '#FAFAFA',
+                        }}
+                        style={{
+                            borderBottomColor: '#F3F3F3',
+                            borderColor: 0,
+                            borderRadius: 0,
+                        }}
+                        labelStyle={{
+                            color: '#414141',
+                            padding: 4,
+
+                        }}
+                        textStyle={{
+                            color: '#414141',
+                            padding: 4,
+                        }}
+                    />
+                    <View>
+                        <AutoHeightTextInput placeholder={"내용을 입력하세요."}/>
+                    </View>
                     <Text style={styles.contentLimit}>0 / 500자</Text>
+                    
                 </View>
             </View>
-        </View>
+        </Pressable>
     );
 }
 
 const styles = StyleSheet.create({
     contentLimit: {
-        textAlign: 'right',
-        paddingRight: 16,
-        paddingTop: 16,
-        color: '#8A8A8A',
-        
-    },
-    content: {
-        height: 500,
-        paddingTop: 16,
-        paddingLeft: 16,
-        paddingRight: 16,
+        position: 'absolute',
+        top: 600,
+        left: 310,
+        // textAlign: 'right',
+        backgroundColor: 'pink',
         paddingBottom: 16,
-        fontSize: 16,
-        borderBottomColor: '#F3F3F3',
-        borderBottomWidth: 1,
+        // paddingTop: 16,
+        color: '#8A8A8A',
     },
     title: {
         padding: 16,
