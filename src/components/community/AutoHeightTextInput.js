71f2ac1f2595e3e440bdb3b6d4ea6e4950b73878
HA JIN <w2128@e-mirim.hs.kr>
2023-05-28 15:19:58 +0900

    ✨: feat : 입력값의 길이에 따라 높이가 자동으로 조정되는 컴포넌트

---
+++ src/components/community/AutoHeightTextInput.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { TextInput } from "react-native";
+import { useState } from "react";
+
+const AutoHeightTextInput = ({placeholder}) => {
+    const [inputHeight, setInputHeight] = useState(0);
+
+    const handleContentSizeChange = (event) => {
+        setInputHeight(event.nativeEvent.contentSize.height);
+    };
+
+    return (
+        <TextInput
+        multiline={true}
+        onContentSizeChange={handleContentSizeChange}
+        placeholder={placeholder}
+        maxLength={500}
+        style={{ height: inputHeight, 
+                minHeight: 60,
+                // backgroundColor: 'pink',
+                paddingTop: 16,
+                paddingLeft: 16,
+                paddingRight: 16,
+                paddingBottom: 16,
+                fontSize: 16,
+        }}
+        />
+    );
+}
+
+export default AutoHeightTextInput;
