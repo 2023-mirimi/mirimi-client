f7905197127c2c5a9a00b4f49b0f6fb1429c941b
hyennin <chm050202@gmail.com>
2023-06-16 14:30:14 +0900

    ✨ feat: 캘린더에 학사일정 띄우기

--- src/components/school/calendarTab.js
+++ src/components/school/calendarTab.js
@@ -1,19 +1,29 @@
+import React, { useState } from 'react';
 import { StyleSheet, View, Image } from 'react-native';
 import { Calendar } from 'react-native-calendars';
 import left from '../../assets/school/left.png';
 import right from '../../assets/school/right.png';
 import data from '../../assets/school/academicCalendar';
+import CalendarArticle from './calendarArticle';
 
 const CalendarTab = () => {
-  
+  const [titleIdx, setTitleIdx] = useState(0);
+  const [selectedDate, setSelectedDate] = useState('');
 
   const handleDayClick = (day) => {
-    data.map((d) => {
-      if (day.dateString === d.date)
-      console.log(d.title[0])
-    })
-    
-  }
+    setSelectedDate(day.dateString);
+    setTitleIdx(0);
+  };
+
+  const renderCalendarArticle = () => {
+    const selectedData = data.find((d) => d.date === selectedDate);
+    if (selectedData) {
+      return selectedData.title.map((title, idx) => (
+        <CalendarArticle key={idx} date={selectedData.date} title={title} />
+      ));
+    }
+    return null;
+  };
 
   return (
     <View style={styles.container}>
@@ -29,15 +39,21 @@ const CalendarTab = () => {
           textMonthFontWeight: 'bold',
           textSectionTitleColor: 'rgba(138, 138, 138, 1)',
         }}
-        onDayPress={(day) => handleDayClick(day)}
+        onDayPress={handleDayClick}
         monthFormat={'M월'}
         hideExtraDays={true}
-        renderArrow={(direction) => direction === "left" ?
-        <Image name="left" size={20} source={left}/> : <Image name="right" size={20} source={right} />}
+        renderArrow={(direction) =>
+          direction === 'left' ? (
+            <Image name="left" size={20} source={left} />
+          ) : (
+            <Image name="right" size={20} source={right} />
+          )
+        }
       />
+      {renderCalendarArticle()}
     </View>
-   )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -45,15 +61,17 @@ const styles = StyleSheet.create({
     paddingLeft: 16,
     paddingRight: 16,
     paddingBottom: 40,
-    backgroundColor: 'rgba(255, 255, 255, 1)'
+    backgroundColor: 'rgba(255, 255, 255, 1)',
   },
   calendar: {
+    width: 343,
     marginTop: 24,
+    marginBottom: 16,
     paddingBottom: 30,
     borderWidth: 1,
     borderColor: '#E9E9E9',
-    borderRadius: 20
-  }
+    borderRadius: 20,
+  },
 });
 
 export default CalendarTab;
