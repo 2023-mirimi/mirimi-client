05a96686628981f7bb374492a15c6925c78f8101
hyennin <chm050202@gmail.com>
2023-06-16 09:29:42 +0900

    ✨ feat: 학사일정 데이터 추가

--- src/components/school/calendarTab.js
+++ src/components/school/calendarTab.js
@@ -1,19 +1,19 @@
 import { StyleSheet, View, Image } from 'react-native';
-import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
+import { Calendar } from 'react-native-calendars';
 import left from '../../assets/school/left.png';
 import right from '../../assets/school/right.png';
-import {LocaleConfig} from 'react-native-calendars';
+import data from '../../assets/school/academicCalendar';
 
-LocaleConfig.locales['fr'] = {
-  monthNames: ['Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre'],
-  monthNamesShort: ['Janv.','Févr.','Mars','Avril','Mai','Juin','Juil.','Août','Sept.','Oct.','Nov.','Déc.'],
-  dayNames: ['일요일','월요일', '화요일','수요일','목요일','금요일','토요일'],
-  dayNamesShort: ['일', '월','화','수','목','금','토'],
-  today: 'Aujourd\'hui'
-};
-LocaleConfig.defaultLocale = 'fr';
+const CalendarTab = () => {
+  
 
-const CalendarTab = () => { 
+  const handleDayClick = (day) => {
+    data.map((d) => {
+      if (day.dateString === d.date)
+      console.log(d.title[0])
+    })
+    
+  }
 
   return (
     <View style={styles.container}>
@@ -29,11 +29,9 @@ const CalendarTab = () => {
           textMonthFontWeight: 'bold',
           textSectionTitleColor: 'rgba(138, 138, 138, 1)',
         }}
-        onDayPress={(day) => {console.log('selected day', day)}}
-        onDayLongPress={(day) => {console.log('selected day', day)}}
+        onDayPress={(day) => handleDayClick(day)}
         monthFormat={'M월'}
         hideExtraDays={true}
-        onMonthChange={(month) => {console.log('month changed', month)}}
         renderArrow={(direction) => direction === "left" ?
         <Image name="left" size={20} source={left}/> : <Image name="right" size={20} source={right} />}
       />
